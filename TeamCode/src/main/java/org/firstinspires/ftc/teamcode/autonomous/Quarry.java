bf02bd1db04bff9ee34e14cc5fd8b9f4d9190160
Simon K <5222sak@gmail.com>
2019-11-17 14:44:41 -0500

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Quarry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Quarry.java
@@ -12,33 +12,39 @@ public class Quarry extends LinearOpMode {
     public void runOpMode(){
         robot.init(hardwareMap);
         waitForStart();
+
+        //Deploy intake wheels
         robot.intake.intakeOn();
         sleep(100);
         robot.intake.intakeOff();
 
-        //drive forward
         robot.dt.strafeToPosition(10,SLOW_SPEED);
 
-        //
         robot.dt.driveToPosition(-12,SLOW_SPEED);
 
+        //turn towards leftmost stone
         robot.gyroTurn(-52);
 
-        //left stone
+        //intakes left stone
         robot.intake.intakeOn();
         robot.dt.driveToPosition(30,SLOW_SPEED);
         robot.intake.intakeOff();
         robot.dt.driveToPosition(-15,SLOW_SPEED);
 
-
+        //turns and moves under skybridge
         robot.gyroTurn(0);
-
         robot.dt.driveToPosition(58,FAST_SPEED);
+
+        //releases stone
         robot.intake.intakeReverse();
         sleep(200);
         robot.intake.intakeOff();
+
+        //zooms back
+        robot.gyroTurn(0);
         robot.dt.driveToPosition(-58,FAST_SPEED);
 
+        //turns towards
         robot.gyroTurn(-120);
         robot.intake.intakeOn();
         robot.dt.driveToPosition(15, SLOW_SPEED);
